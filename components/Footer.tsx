f10b1e7b03e978770cbe34d157ee633e92443976
jokerhubo <jokerhubo@hotmail.com>
2023-03-30 19:25:59 +0800

    first  try

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -35,7 +35,7 @@ export const FooterImpl: React.FC = () => {
 
   return (
     <footer className={styles.footer}>
-      <div className={styles.copyright}>Copyright 2022 {config.author}</div>
+      <div className={styles.copyright}>版权所有 2023 {config.author}</div>
 
       <div className={styles.settings}>
         {hasMounted && (
